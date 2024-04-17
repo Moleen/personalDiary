1690a879fdcd63f8b48a264273d38f71e71f831c
Moleen <maulana.syakhiya@gmail.com>
2024-04-18 04:00:01 +0700

    .

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+import os
+from os.path import join, dirname
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
 from datetime import datetime
+from dotenv import load_dotenv
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
 app = Flask(__name__)
-client = MongoClient('mongodb+srv://maulanasyakhiya:X6Tx5vkB5TZUiCMo@cluster0.zsgjrxk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-db = client.dbsparta
 @app.route('/')
 def home():
     return render_template('index.html')
